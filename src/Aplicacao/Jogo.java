d22dff9d025960d2c9d21d33dd16e76caa530cfc
João Pedro <depgon2402@gmail.com>
2025-11-25 23:57:51 -0300

    Atualização do projeto: melhorias no prompt e correção de erros

--- src/Aplicacao/Jogo.java
+++ src/Aplicacao/Jogo.java
@@ -12,7 +12,7 @@ public class Jogo {
 		
 		System.out.println("Bem-vindo ao Terminal Quest!");
 		
-		System.out.print("Digite o nome do seu herói: ");
+		System.out.println("Digite o nome do seu herói: ");
 		String nomeHeroi = sc.nextLine();
 		
 		Heroi meuheroi = new Heroi(nomeHeroi, 100, 20, 10);
@@ -23,12 +23,12 @@ public class Jogo {
 		
 		while(jogoAtivo && meuheroi.estaVivo()) {
 			
-			System.out.println("/n---Acampamento---");
+			System.out.println("--- ACAMPAMENTO ---");
 			System.out.println("1. Explorar Dungeon");
 			System.out.println("2. Descansar (Recuperar vida)");
 			System.out.println("3. Ver status do herói");
 			System.out.println("4. Sair");
-			System.out.println("Digite a opção: ");
+			System.out.println("-------------------");
 			int opcao = sc.nextInt();
 			
 			switch(opcao) {	
@@ -40,14 +40,14 @@ public class Jogo {
 				System.out.println(meuheroi.getNome() + " descansou e recuperou vida.");
 				break;
 			case 3:
-				System.out.println("\n---Status do Herói---");
+				System.out.println("---- Status do Herói ----");
 				System.out.println("Nome: " + meuheroi.getNome());
 				System.out.println("Vida: " + meuheroi.getVida() + "/" + meuheroi.getMaxvida());
 				System.out.println("Força: " + meuheroi.getForca());
 				System.out.println("Defesa: " + meuheroi.getDefesa());
 				System.out.println("Nível: " + meuheroi.getNivel());
 				System.out.println("Experiência: " + meuheroi.getExp() + "/100");
-				System.out.println("---------------------");
+				System.out.println("-----------------------");
 				break;
 			case 4:
 				jogoAtivo = false;
@@ -73,10 +73,12 @@ public class Jogo {
 		System.out.println("Um " + monstro.getNome() + " apareceu!");
 		
 		while(heroi.estaVivo() && monstro.estaVivo()) {
+			System.out.println("-----------------------");
 			System.out.println("\n" + heroi.getNome() + " - Vida: " + heroi.getVida());
 			System.out.println(monstro.getNome() + " - Vida: " + monstro.getVida());
 			System.out.println("1. Atacar");
 			System.out.println("2. Fugir");
+			System.out.println("-----------------------");
 			System.out.println("/nEscolha sua ação: ");
 			
 			int escolha = sc.nextInt();
@@ -88,23 +90,30 @@ public class Jogo {
 
 			    //Turno do Monstro
 			    if (monstro.estaVivo()) {
+			
 			    	System.out.println("\nO " + monstro.getNome() + " contra-ataca!");
 			        int danoMonstro = monstro.atacar();
 			        heroi.receberDano(danoMonstro);
+			        System.out.println("-----------------------");
 			        
 			        if (!heroi.estaVivo()) {
+			        	System.out.println("-----------------------");
 			            System.out.println(heroi.getNome() + " foi derrotado pelo " + monstro.getNome() + ", a dungeon venceu!");
+			            System.out.println("-----------------------");
 			        }
 			       
 			    }
 			    // Resultados da batalha
 			    else {
+			    	System.out.println("-----------------------");
 			        System.out.println(heroi.getNome() + " derrotou o " + monstro.getNome() + "!");
 			        heroi.ganharExp(50);
 			        System.out.println(heroi.getNome() + " ganhou 50 pontos de experiência.");
+			        System.out.println("-----------------------");
 			    	} 
 				}
 				else if (escolha == 2) {
+					
 				System.out.println("Você fugiu da batalha!");
 				break;
 				}
